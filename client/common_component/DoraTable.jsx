7a74dfba4c9eb0c72867fb0445bcabd38e7dac8a
jhonyoung <814323375@qq.com>
2017-01-05 20:10:33 +0800

    public component

--- client/common_component/DoraTable.jsx
+++ client/common_component/DoraTable.jsx
@@ -1,5 +1,10 @@
-// import { Table } from 'antd';
-// import React, { Component } from 'react';
+import { Table } from 'antd';
+import React, { Component } from 'react';
 
-// let DoraTable = React.createClass ({
-// })
+let DoraTable = React.createClass ({
+	render() {
+		return (<div>test</div>)
+	}
+})
+
+export default DoraTable;
